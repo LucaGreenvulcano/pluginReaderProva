8aa4b64f5c3138af00dd18fd11447a7aaeda35d5
Luca Corcione <l.corcione@greenvulcano.com>
2019-01-03 15:53:29 +0100

    prova di push

--- android/src/main/java/io/cmichel/boilerplate/Module.java
+++ android/src/main/java/io/cmichel/boilerplate/Module.java
@@ -9,10 +9,27 @@ import com.facebook.react.bridge.ReactMethod;
 import java.util.HashMap;
 import java.util.Map;
 
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
 public class Module extends ReactContextBaseJavaModule {
 
   private static final String DURATION_SHORT_KEY = "SHORT";
   private static final String DURATION_LONG_KEY = "LONG";
+  private NFCReader mNfcReadFragment;
+
 
   public Module(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -34,5 +51,14 @@ public class Module extends ReactContextBaseJavaModule {
   @ReactMethod
   public void show(String message, int duration) {
     Toast.makeText(getReactApplicationContext(), message, duration).show();
+  }
+
+  @ReactMethod
+  public void testRead(){
+      mNfcReadFragment = (NFCReader) getFragmentManager().findFragmentByTag(NFCReader.TAG);
+      if (mNfcReadFragment == null) {
+        mNfcReadFragment = NFCReader.newInstance();
+      }
+      this.show(getFragmentManager(),NFCReadFragment.TAG);
   }
 }
