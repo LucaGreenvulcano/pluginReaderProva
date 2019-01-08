09686c50d9ce1675a658bdbaad816e46421b1d1b
Luca Corcione <l.corcione@greenvulcano.com>
2019-01-08 17:48:14 +0100

    again

--- android/src/main/java/io/cmichel/boilerplate/Module.java
+++ android/src/main/java/io/cmichel/boilerplate/Module.java
@@ -2,6 +2,10 @@ package io.cmichel.boilerplate;
 
 import android.widget.Toast;
 
+package io.cmichel.boilerplate;
+
+import android.widget.Toast;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -9,37 +13,21 @@ import com.facebook.react.bridge.ReactMethod;
 import java.util.HashMap;
 import java.util.Map;
 
-
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.nfc.NfcAdapter;
-import android.nfc.Tag;
-import android.nfc.tech.Ndef;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-
 public class Module extends ReactContextBaseJavaModule {
 
   private static final String DURATION_SHORT_KEY = "SHORT";
   private static final String DURATION_LONG_KEY = "LONG";
 
-
   public Module(ReactApplicationContext reactContext) {
     super(reactContext);
   }
 
-
+  @Override
   public String getName() {
     return "Boilerplate";
   }
 
-
+  @Override
   public Map<String, Object> getConstants() {
     final Map<String, Object> constants = new HashMap<>();
     constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
@@ -47,7 +35,7 @@ public class Module extends ReactContextBaseJavaModule {
     return constants;
   }
 
-
+  @ReactMethod
   public void show(String message, int duration) {
     Toast.makeText(getReactApplicationContext(), message, duration).show();
   }
