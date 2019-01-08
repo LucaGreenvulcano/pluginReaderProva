d86f37fb9fd038823f86c61e8c52cd79f16c359f
Luca Corcione <l.corcione@greenvulcano.com>
2019-01-08 10:15:23 +0100

    prova 8 gennaio 2019

--- index.android.js
+++ index.android.js
@@ -3,3 +3,7 @@
 import { NativeModules } from 'react-native'
 // name as defined via ReactContextBaseJavaModule's getName
 module.exports = NativeModules.Boilerplate
+@Override
+public String getName() {
+    return "Boilerplate";
+}
