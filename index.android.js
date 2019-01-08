bec1d52bfbe02a6c76edd618139d5ded9844a3fb
Luca Corcione <l.corcione@greenvulcano.com>
2019-01-08 17:16:51 +0100

    again

--- index.android.js
+++ index.android.js
@@ -3,7 +3,3 @@
 import { NativeModules } from 'react-native'
 // name as defined via ReactContextBaseJavaModule's getName
 module.exports = NativeModules.Boilerplate
-
-public String getName() {
-    return "Boilerplate";
-}
