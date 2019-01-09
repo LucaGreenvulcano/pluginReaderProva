470d4ad52d7617b9690bd2486c50fda051a33e03
Luca Corcione <l.corcione@greenvulcano.com>
2019-01-09 10:33:15 +0100

    try again

--- android/src/main/java/io/cmichel/boilerplate/Module.java
+++ android/src/main/java/io/cmichel/boilerplate/Module.java
@@ -35,4 +35,9 @@ public class Module extends ReactContextBaseJavaModule {
   public void show(String message, int duration) {
     Toast.makeText(getReactApplicationContext(), message, duration).show();
   }
+
+  @ReactMethod
+  public void prova(String message, int duration) {
+    Toast.makeText(getReactApplicationContext(), message, duration).show();
+  }
 }
