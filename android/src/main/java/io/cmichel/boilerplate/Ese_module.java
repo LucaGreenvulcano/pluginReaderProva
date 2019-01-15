fc37f8aaedd0a3ae566bfc9624311006b695fd5b
Luca Corcione <l.corcione@greenvulcano.com>
2019-01-15 17:02:16 +0100

    commit del 15 gennaio 2019 001

--- android/src/main/java/io/cmichel/boilerplate/Ese_module.java
+++ android/src/main/java/io/cmichel/boilerplate/Ese_module.java
@@ -9,37 +9,20 @@ import com.facebook.react.bridge.ReactMethod;
 import java.util.HashMap;
 import java.util.Map;
 
-
-//NFC
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.nfc.NfcAdapter;
-import android.nfc.Tag;
-import android.nfc.tech.Ndef;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-
-
-public class Module extends ReactContextBaseJavaModule {
+public class Ese_module extends ReactContextBaseJavaModule {
 
   private static final String DURATION_SHORT_KEY = "SHORT";
   private static final String DURATION_LONG_KEY = "LONG";
 
   private NFCReadFragment mNfcReadFragment;
 
-  public Module(ReactApplicationContext reactContext) {
+  public Ese_module(ReactApplicationContext reactContext) {
     super(reactContext);
   }
 
   @Override
   public String getName() {
-    return "Boilerplate";
+    return "Boilerplate_due";
   }
 
   @Override
@@ -57,11 +40,11 @@ public class Module extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void prova() {
-     IntentView intent;
+/*
       Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
       Ndef ndef = Ndef.get(tag);
       mNfcReadFragment.onNfcDetected(ndef);
-
+*/
       Toast.makeText(getReactApplicationContext(), "tutto bene", 1000).show();
   }
 }
