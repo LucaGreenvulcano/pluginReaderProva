651a85ae6bc9324294235c820217552a90d2aace
Luca Corcione <l.corcione@greenvulcano.com>
2019-01-30 15:23:39 +0100

    30 gennaio inserimento toast

--- android/src/main/java/prova/plugin/boilnfc/NfcManager.java
+++ android/src/main/java/prova/plugin/boilnfc/NfcManager.java
@@ -155,7 +155,7 @@ class NfcManager extends ReactContextBaseJavaModule implements ActivityEventList
 	public void show() {
 		String message = "prova toast";
 		int duration = 1000;
-		Toast.makeText(getReactApplicationContext(), message, duration).show();
+//		Toast.makeText(getReactApplicationContext(), message, duration).show();
 	}
 
 	@ReactMethod
