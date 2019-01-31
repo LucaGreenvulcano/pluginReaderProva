1388710219c8ca24148c4123ab09ef26c7d57608
Luca Corcione <l.corcione@greenvulcano.com>
2019-01-31 11:23:09 +0100

    prima prova di inserimento librerie nxp 001

--- android/src/main/java/prova/plugin/boilnfc/Module.java
+++ android/src/main/java/prova/plugin/boilnfc/Module.java
@@ -12,17 +12,6 @@ import java.util.Map;
 //-------------------
 //      /*
 
-import android.nfc.NdefRecord;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.MenuItem;
-import android.widget.TextView;
-import android.content.Intent;
 
 import com.nxp.nfclib.CardType;
 import com.nxp.nfclib.NxpNfcLib;
@@ -40,7 +29,7 @@ import com.nxp.nfclib.utils.NxpLogUtils;
 import com.nxp.nfclib.utils.Utilities;
 
 
-//      +/
+//      */
 //----------------------
 
 public class Module extends ReactContextBaseJavaModule {
