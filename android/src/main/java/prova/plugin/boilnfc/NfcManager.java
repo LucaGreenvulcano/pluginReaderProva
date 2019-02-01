2c412b7d56cb050096fdd9b2d8b87f4e33fe6b26
Luca Corcione <l.corcione@greenvulcano.com>
2019-02-01 15:17:19 +0100

    NXP importazione libreria dichiarazione variabili 004

--- android/src/main/java/prova/plugin/boilnfc/NfcManager.java
+++ android/src/main/java/prova/plugin/boilnfc/NfcManager.java
@@ -86,7 +86,6 @@ class NfcManager extends ReactContextBaseJavaModule implements ActivityEventList
 	// private String TAG = MainActivity.class.getSimpleName();
 	private String TAG = NfcManager.class.getSimpleName();
 	private NxpNfcLib m_libInstance = null;  // The TapLinX library instance
-	private TextView mTextMessage;
 	private final String TAPLINX_KEY = "07698d782c5070246b1df1b1ed79adde";
 	//    private final byte[] pwd = new byte[]{0x01, 0x02, 0x03, 0x04};
 	private final byte[] pwd = new byte[]{0x31, 0x32, 0x33, 0x34};
